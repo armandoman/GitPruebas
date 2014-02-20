864bbd4304aa9e22c41ba1b312d8b84d9c9206f2
Armando <kakumate.34@hotmail.com>
2014-02-20 15:53:36 -0600

    unas practica mas elaborada

---
+++ Pk2_RSVPwapp/Pk2_RSVPwapp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pk2_RSVPwapp.Models;
+
+namespace Pk2_RSVPwapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ?
+                "Buenos dias!!!!" : "BUenas tardes!!!";
+            return View();
+        }
+        //Get :/Home/RsvpForm
+        // Renderea el link de la forma del RSVP
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        //Responde a un Post: /Home/RsvpForm
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //Todo: Enviar a un Post:/Home/RsvpForm
+            return View("Agradecimientos", guestResponse);
+        }
+    }
+}
---
+++ Pk2_RSVPwapp/Pk2_RSVPwapp/Models/GuestResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pk2_RSVPwapp.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
