a1a5be1d0117a4f771603a0b73a23e367a2323e0
Armando <kakumate.34@hotmail.com>
2014-03-20 22:30:12 -0600

    iniciando me has visto

---
+++ MeHasVisto/MeHasVisto/Controllers/PetController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeHasVisto.Controllers
+{
+    public class PetController : Controller
+    {
+        //
+        // GET: /Pet/
+
+        public  ActionResult Display()
+        {
+            var name = (string)RouteData.Values["id"];
+            //var model = PetManagement.GetByBame(name);
+            //if (model == null)
+                return RedirectToAction("NotFound");
+            //return View(model);
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+        public FilePathResult DowLoadPicture()
+        {
+            var name = (string)RouteData.Values["id"];
+            var picture = "/Content/Uploads" + name + ".jpg";
+            var contentType = "image/jpg";
+            var fileName = name + ".jpg";
+            return File(picture, contentType, fileName);
+        }
+
+    }
+}
